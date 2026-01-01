2700e36208e7573fd92cc8e7178fb05ba6281c40
unknown <wirawanaryo99@gmail.com>
2026-01-01 22:25:58 +0700

    finished all exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+  let n = Number(nth);
+  if (n < 0) {
+    return "OOPS";
+  }else{
+    if (n === 0) {
+      return 0;
+    }else if (n === 1) {
+      return 1;
+    }else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person)=> {
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    return personAge > oldestAge ? person : oldest;
+  });
+  // const currentYear = new Date().getFullYear();
+  // return people.reduce((oldest, person) => {
+  //   const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+  //   const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+  //   return personAge > oldestAge ? person : oldest;
+  // });
 };
 
 // Do not edit below this line
